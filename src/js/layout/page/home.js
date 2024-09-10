ce553ad63536566b212be7e3b217059f1215eea5
Max V Grushin <support@ids.spb.ru>
2024-09-10 11:13:03 +0300

    feat: добавлена секция выполненные работы

---
+++ src/js/layout/page/home.js
@@ -0,0 +1,49 @@
+import Swiper from 'swiper';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+
+import {
+  Navigation,
+  Autoplay,
+  Mousewheel,
+  Pagination,
+  Thumbs,
+  Controller,
+  EffectFade,
+} from "swiper/modules";
+
+new Swiper(".our-works .swiper", {
+  modules: [Navigation, Autoplay],
+
+  slidesPerView: 1,
+  spaceBetween: 26,
+  loop: true,
+  speed: 2000,
+
+  breakpoints: {
+    260: {
+      slidesPerView: 1,
+    },
+    360: {
+      slidesPerView: 2,
+    },
+    740: {
+      slidesPerView: 3,
+    },
+    1200: {
+      slidesPerView: 4,
+    },
+  },
+
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+
+  navigation: {
+    nextEl: ".our-works__slider .swiper-button-next",
+    prevEl: ".our-works__slider .swiper-button-prev",
+  },
+
+});
