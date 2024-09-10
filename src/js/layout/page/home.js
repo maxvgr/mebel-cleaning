68b3cf916033a0632b4b4e57af84a5122c6893be
Max V Grushin <support@ids.spb.ru>
2024-09-10 13:37:47 +0300

    feat: добавлена секция отзывы

--- src/js/layout/page/home.js
+++ src/js/layout/page/home.js
@@ -47,3 +47,38 @@ new Swiper(".our-works .swiper", {
   },
 
 });
+
+new Swiper(".reviews .swiper", {
+  modules: [Navigation, Autoplay],
+
+  slidesPerView: 1,
+  spaceBetween: 26,
+  loop: true,
+  speed: 2000,
+
+  breakpoints: {
+    260: {
+      slidesPerView: 1,
+    },
+    360: {
+      slidesPerView: 2,
+    },
+    740: {
+      slidesPerView: 3,
+    },
+    1200: {
+      slidesPerView: 3,
+    },
+  },
+
+  autoplay: {
+    delay: 5000,
+    disableOnInteraction: false,
+  },
+
+  navigation: {
+    nextEl: ".reviews__wrapper .swiper-button-next",
+    prevEl: ".reviews__wrapper .swiper-button-prev",
+  },
+
+});
