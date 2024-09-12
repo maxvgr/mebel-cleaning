26f0d45c295840b0387daad95608d839c86156de
Max V Grushin <support@ids.spb.ru>
2024-09-12 12:11:22 +0300

    feat: добавлен адаптив шапки

--- src/js/layout/header.js
+++ src/js/layout/header.js
@@ -1,6 +1,17 @@
 import { Fancybox } from "@fancyapps/ui";
 import "@fancyapps/ui/dist/fancybox/fancybox.css";
 
+const hamburger = document.querySelector(".hamburger--spin");
+const mobileNavs = document.querySelector(".header__menu-mobile");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("is-active");
+  mobileNavs.classList.toggle("is-active");
+  // overlay.classList.toggle("is-active");
+});
+
+
+
 Fancybox.bind("[data-fancybox]", {
   // Your custom options
 });
